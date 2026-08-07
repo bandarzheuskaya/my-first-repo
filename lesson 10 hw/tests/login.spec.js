e23e20d60f35c4d4de37405988d4bb1f8cc890d6
bandarzheuskaya <cougar.anasta.06@gmail.com>
2026-08-07 16:07:36 +0300

    feat: add first Playwright tests

---
+++ lesson 10 hw/tests/login.spec.js
@@ -0,0 +1,29 @@
+import {test, expect} from '@playwright/test'
+
+test.describe('Авторизация на SauseDemo', () => {
+
+    test('Успешная авторизация', async ({page}) => {
+        await page.goto('https://www.saucedemo.com/');
+
+        await page.getByRole('textbox', {name: 'Username'}).fill('standard_user');
+
+        await page.locator('#password').fill('secret_sauce');
+
+        await page.locator('[data-test="login-button"]').click();
+
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html')
+
+    })
+
+    test('Неуспешная авторизация с заблокированным пользователем', async ({page}) => {
+        await page.goto('https://www.saucedemo.com/');
+
+        await page.getByRole('textbox', {name: 'Username'}).fill('locked_out_user');
+
+        await page.locator('#password').fill('secret_sauce');
+
+        await page.locator('[data-test="login-button"]').click();
+
+        await expect(page.locator('[data-test="error"]')).toHaveText('Epic sadface: Sorry, this user has been locked out.')
+    })
+})
